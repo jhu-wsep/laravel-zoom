8aac9d95178c59b98fdd9b3f8194a663b666a4d5
Tim Jarrett <tim.jarrett@gmail.com>
2021-03-08 11:17:07 -0500

    Adds PHPDoc comments to help with IDE hinting

--- src/MeetingSetting.php
+++ src/MeetingSetting.php
@@ -4,6 +4,17 @@ namespace MacsiDigital\Zoom;
 
 use MacsiDigital\Zoom\Support\Model;
 
+/**
+ * @property bool $allow_multiple_devices
+ * @property string $alternative_hosts  Alternative host's emails or IDs: multiple values separated by a comma
+ * @property int $approval_type
+ * @property array $approved_or_denied_countries_or_regions
+ * @property string $audio
+ * @property string $auto_recording
+ * @property bool $host_video Start video when the host joins the meeting.
+ * @property bool $participant_video Start video when the participants joins the meeting.
+ *
+ */
 class MeetingSetting extends Model
 {
     public function globalDialInNumbers()
